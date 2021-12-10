01aa5da5d4e928384492f932bb5f5e1292359caa
Lina NEGOUAI <lina.negouai@wanadoo.fr>
2021-12-10 17:24:09 -0400

    ReviewsController en chantier

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+babaorum = Restaurant.new(name: 'Babaorum', address: '42, rue de Chateauboeuf', category: 'french')
+babaorum.save!
+cantonnais = Restaurant.new(name: 'Baguettes cantonnaises', address: '47 route de Cluny, Fort-de-France', category: 'chinese')
+cantonnais.save!
+pizz = Restaurant.new(name: "Acro' Pizz", address: 'quartier Poterie, Trois Ilets', category: 'italian')
+pizz.save!
+torii = Restaurant.new(name: 'Torii Sushi', address: 'Schoelcher', category: 'japanese')
+torii.save!
+phare = Restaurant.new(name: 'Le Phare', address: 'Tartane, Trinité', category: 'french')
+phare.save!
