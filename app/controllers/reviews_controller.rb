55ae84c0bcfc7dcd2c6642304846955f52cedaf1
Lina NEGOUAI <lina.negouai@wanadoo.fr>
2021-12-11 14:02:40 -0400

    controleurs et vues ok. Manque style

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,8 +5,8 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    @review = Review.new(review_params)
     @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
     @review.restaurant = @restaurant
     @review.save
     redirect_to restaurant_path(@restaurant)
